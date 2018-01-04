07683704385fd53fccfc298602086ecef4372ab4
AmolBhagat <amol.dhanaji.bhagat@gmail.com>
2018-01-04 09:07:45 +0530

    First Commit

---
+++ Method Overriding/Method Overriding/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overriding
+{
+    class BaseClass
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("I am a base class print method");
+        }
+    }
+    class DerivedClass : BaseClass
+    {
+        public override void Print()
+        {
+            Console.WriteLine("I am a derived class print method");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass B = new DerivedClass();
+            B.Print();
+            Console.ReadKey();
+        }
+    }
+}
