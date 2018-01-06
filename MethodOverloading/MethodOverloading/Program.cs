c8b1140af85a144cc7a2d2103168a7854a7ee0e4
AmolBhagat <amol.dhanaji.bhagat@gmail.com>
2018-01-06 09:54:56 +0530

    First Commit

---
+++ MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Product = 0;
+            Add(10, 20);
+            Add(5, 10,out Product);
+            Add(10, 20, 30);
+            Console.ReadKey();
+        }
+
+        public static void Add(int FN,int SN)
+        {
+            Console.WriteLine("Sum = {0}", FN + SN);
+        }
+        public static void Add(int FN, int SN, out int product)
+        {
+            Console.WriteLine("Sum = {0}", FN * SN);
+            product = FN * SN;
+
+        }
+        public static void Add(int FN, int SN,int TN)
+        {
+            Console.WriteLine("Sum = {0}", FN + SN + TN);
+        }
+    }
+}
