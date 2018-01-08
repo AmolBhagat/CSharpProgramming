7b1eddd6ba13811bffc7ab6440c1a81c3c026ea2
AmolBhagat <amol.dhanaji.bhagat@gmail.com>
2018-01-08 16:50:28 +0530

    First Commit

---
+++ Abstarct Classes/Abstarct Classes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstarct_Classes
+{
+    public abstract class Customer
+    {
+        public abstract void Print1();  //Abstract method
+
+        public void Print2() //non abstarct method
+        {
+            Console.WriteLine("Non abstract method Print2 method");
+        }
+    }
+    class Program:Customer
+    {
+
+        static void Main(string[] args)
+        {
+            Program P = new Program();
+            P.Print1();
+
+            Customer C1 = new Program(); //Abstract class reference variable can point to the derived class object.
+            C1.Print2();
+            Console.ReadKey();
+        }
+
+        public override void Print1()
+        {
+            Console.WriteLine("Abstract method Print1 method");
+        }
+    }
+}
