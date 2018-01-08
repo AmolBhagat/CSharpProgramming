83bef4263ed9f1891195b76f35f819752ac873e5
AmolBhagat <amol.dhanaji.bhagat@gmail.com>
2018-01-08 15:26:30 +0530

    First Commit

---
+++ Introduction to Interfaces/Introduction to Interfaces/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction_to_Interfaces
+{
+    interface ICustomer1
+    {     
+        void Print1();
+    }
+    interface ICustomer2:ICustomer1
+    {
+        void Print2();
+    }
+    public class Customer : ICustomer2
+    {
+        public void Print1()
+        {
+            Console.WriteLine("Prit1 Method");
+        }
+
+        public void Print2()
+        {
+            Console.WriteLine("Prit2 Method");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer C1 = new Customer();
+            C1.Print1();
+            C1.Print2();
+
+            ICustomer1 cust = new Customer();
+            cust.Print1();
+            Console.ReadKey();
+        }
+    }
+}
