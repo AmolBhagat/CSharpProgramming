4d29cef9919ffd454018f2198e88d4e2a0221a12
AmolBhagat <amol.dhanaji.bhagat@gmail.com>
2018-01-10 14:47:49 +0530

    First Commit

---
+++ Exception Handling/Exception Handling/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader streamReader = null;
+            try
+            {
+                streamReader = new StreamReader(@"E:\All DOTNET Training Videos\CsharpVideos\Sample Files\Data.txt");
+                Console.WriteLine(streamReader.ReadToEnd());
+                streamReader.Close();
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine("Please check if the file {0} exists",ex.FileName);
+                
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (streamReader != null)
+                {
+                    streamReader.Close();
+                }
+                Console.WriteLine("Finally Block");
+            }
+            Console.ReadKey();
+        }
+    }
+}
