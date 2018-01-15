a1f320a23a037bbf34b8c439b0b57192d093ac68
AmolBhagat <amol.dhanaji.bhagat@gmail.com>
2018-01-15 14:05:48 +0530

    First Commit

---
+++ CustomExceptions/CustomExceptions/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new UserAlreadyLoggedInException("User Already logged in - no duplicate session is allowed");
+            }
+            catch (UserAlreadyLoggedInException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+
+    [Serializable]
+    public class UserAlreadyLoggedInException : Exception
+    {
+        public UserAlreadyLoggedInException(string message)
+            : base(message)
+        {
+        }
+
+
+        public UserAlreadyLoggedInException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public UserAlreadyLoggedInException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
