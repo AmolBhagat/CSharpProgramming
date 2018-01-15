0e551cef741e43450038fd562d7f640fe2015b8e
AmolBhagat <amol.dhanaji.bhagat@gmail.com>
2018-01-15 16:47:35 +0530

    First Commit

---
+++ Enums Tutorial/Enums Tutorial/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] Values = (long[])Enum.GetValues(typeof(Gender));
+            foreach(int value in Values)
+            {
+                Console.WriteLine(value);
+            }
+
+            string[] Names = Enum.GetNames(typeof(Gender));
+            foreach (string names in Names)
+            {
+                Console.WriteLine(names);
+            }
+            Console.ReadKey();
+        }
+    }
+    public enum Gender : long
+    {
+        Unknown = 1,
+        Male = 2,
+        Female =3
+    }
+}
